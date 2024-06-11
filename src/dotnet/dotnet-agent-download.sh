7108a60f9ea6518720a116e38e1c20ee5505f10f
Tim Pansino <timpansino@gmail.com>
2024-06-10 17:14:45 -0700

    Get java and dotnet downloads working

---
+++ src/dotnet/dotnet-agent-download.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+TARGETARCH=$1
+AGENT_VERSION=$2
+
+# If the input arg is empty...
+if [[ -z "$AGENT_VERSION" ]]; then
+    # Download current .NET agent version if no argument is provided
+    wget -c "https://download.newrelic.com/dot_net_agent/latest_release/newrelic-dotnet-agent_${TARGETARCH}.tar.gz" -O - | tar -xz --strip-components 1
+else
+    # Download the .NET agent version specified by the provided input argument
+    wget -c "https://download.newrelic.com/dot_net_agent/previous_releases/${AGENT_VERSION}/newrelic-dotnet-agent_${AGENT_VERSION}_${TARGETARCH}.tar.gz" -O - | tar -xz --strip-components 1
+fi
